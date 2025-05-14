a03a926c7df36be2e0ebe6a62c2224c440b204d4
Dj1752 <83537437+Dj1752@users.noreply.github.com>
2025-05-14 23:47:02 +0530

    Simple UI Design ViewElements and Intents

--- app/src/main/java/com/pratice/android_tutorial/MainActivity.kt
+++ app/src/main/java/com/pratice/android_tutorial/MainActivity.kt
@@ -2,6 +2,7 @@ package com.pratice.android_tutorial
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
@@ -9,7 +10,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId")
+    @SuppressLint("MissingInflatedId", "SuspiciousIndentation")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -18,25 +19,43 @@ class MainActivity : AppCompatActivity() {
         val RelativelLayoutbtn: Button = findViewById(R.id.Relativelayoutbt)
         val ConstraintLayouts: Button = findViewById(R.id.constraintlayoutbt)
         val FrameLayouts: Button = findViewById(R.id.framelayoutbt)
+        val viewElementlayoutbt: Button = findViewById(R.id.viewElementlayoutbt)
+        val implicitbt: Button = findViewById(R.id.implicitbt)
+
+
         linearLayoutbtn.setOnClickListener{
-         val intent = Intent(this,LinearLayouts::class.java)
+         val intent = Intent(this,LinearLayouts::class.java)//Explicit Intent
             startActivity(intent)
             Toast.makeText(this,"Clicked",Toast.LENGTH_SHORT).show()
         }
         RelativelLayoutbtn.setOnClickListener{
-         val intent = Intent(this,RelativelLayouts::class.java)
+         val intent = Intent(this,RelativelLayouts::class.java)//Explicit Intent
             startActivity(intent)
             Toast.makeText(this,"Clicked",Toast.LENGTH_SHORT).show()
         }
         ConstraintLayouts.setOnClickListener{
-         val intent = Intent(this,ConstraintLayout::class.java)
+         val intent = Intent(this,ConstraintLayout::class.java)//Explicit Intent
             startActivity(intent)
             Toast.makeText(this,"Clicked",Toast.LENGTH_SHORT).show()
         }
         FrameLayouts.setOnClickListener{
-         val intent = Intent(this,FrameLayout::class.java)
+         val intent = Intent(this,FrameLayout::class.java)//Explicit Intent
             startActivity(intent)
             Toast.makeText(this,"Clicked",Toast.LENGTH_SHORT).show()
         }
+        viewElementlayoutbt.setOnClickListener{
+         val intent = Intent(this,ViewElements::class.java)//Explicit Intent
+            startActivity(intent)
+            Toast.makeText(this,"Clicked",Toast.LENGTH_SHORT).show()
+        }
+
+        implicitbt.setOnClickListener{
+            val url = "https://www.geeksforgeeks.org/implicit-and-explicit-intents-in-android-with-examples/"
+         val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))//Implicit Intent
+            startActivity(intent)
+            Toast.makeText(this,"Clicked",Toast.LENGTH_SHORT).show()
+        }
+
     }
+
 }
