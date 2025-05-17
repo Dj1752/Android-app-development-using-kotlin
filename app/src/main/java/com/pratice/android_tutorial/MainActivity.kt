64cf6ffe89fa052a187ca48c24d7256f526a78fd
Dj1752 <83537437+Dj1752@users.noreply.github.com>
2025-05-18 00:37:03 +0530

    Simple UI Design Navigation Drawer Android Kotlin

--- app/src/main/java/com/pratice/android_tutorial/MainActivity.kt
+++ app/src/main/java/com/pratice/android_tutorial/MainActivity.kt
@@ -1,134 +1,53 @@
 package com.pratice.android_tutorial
 
 import android.annotation.SuppressLint
-import android.app.Dialog
-import android.content.Intent
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
-import android.net.Uri
 import android.os.Bundle
-import android.provider.CalendarContract.Colors
-import android.view.Window
-import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.bottomsheet.BottomSheetDialog
-import com.pratice.android_tutorial.fragment.FragmentPage
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+
+import com.pratice.android_tutorial.fragment.HomeFragment
 
 class MainActivity : AppCompatActivity() {
+    lateinit var drawerLayout: DrawerLayout
+    lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
+
     @SuppressLint("MissingInflatedId", "SuspiciousIndentation")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        val linearLayoutbtn: Button = findViewById(R.id.Linearlayoutbt)
-        val RelativelLayoutbtn: Button = findViewById(R.id.Relativelayoutbt)
-        val ConstraintLayouts: Button = findViewById(R.id.constraintlayoutbt)
-        val FrameLayouts: Button = findViewById(R.id.framelayoutbt)
-        val viewElementlayoutbt: Button = findViewById(R.id.viewElementlayoutbt)
-        val implicitbt: Button = findViewById(R.id.implicitbt)
-        val ListViewbt: Button = findViewById(R.id.ListViewbt)
-        val CustListViewbt: Button = findViewById(R.id.CustListViewbt)
-        val RecycleViewbt: Button = findViewById(R.id.RecycleViewbt)
-        val Fragmentlayoutbt: Button = findViewById(R.id.Fragmentlayoutbt)
-        val Customdialogbt: Button = findViewById(R.id.Customdialogbt)
-        val BottomSheetdialogbt: Button = findViewById(R.id.BottomSheetdialogbt)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        drawerLayout = findViewById(R.id.drawer_layout)
+        actionBarDrawerToggle = ActionBarDrawerToggle(this, drawerLayout,toolbar, R.string.nav_open, R.string.nav_close)
+        drawerLayout.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
 
-
-        linearLayoutbtn.setOnClickListener{
-         val intent = Intent(this,LinearLayouts::class.java)//Explicit Intent
-            startActivity(intent)
-            Toast.makeText(this,"Clicked",Toast.LENGTH_SHORT).show()
-        }
-        RelativelLayoutbtn.setOnClickListener{
-         val intent = Intent(this,RelativelLayouts::class.java)//Explicit Intent
-            startActivity(intent)
-            Toast.makeText(this,"Clicked",Toast.LENGTH_SHORT).show()
-        }
-        ConstraintLayouts.setOnClickListener{
-         val intent = Intent(this,ConstraintLayout::class.java)//Explicit Intent
-            startActivity(intent)
-            Toast.makeText(this,"Clicked",Toast.LENGTH_SHORT).show()
-        }
-        FrameLayouts.setOnClickListener{
-         val intent = Intent(this,FrameLayout::class.java)//Explicit Intent
-            startActivity(intent)
-            Toast.makeText(this,"Clicked",Toast.LENGTH_SHORT).show()
-        }
-        viewElementlayoutbt.setOnClickListener{
-         val intent = Intent(this,ViewElements::class.java)//Explicit Intent
-            startActivity(intent)
-            Toast.makeText(this,"Clicked",Toast.LENGTH_SHORT).show()
-        }
-        ListViewbt.setOnClickListener{
-         val intent = Intent(this,ListViewfile::class.java)//Explicit Intent
-            startActivity(intent)
-            Toast.makeText(this,"Clicked",Toast.LENGTH_SHORT).show()
-        }
-        CustListViewbt.setOnClickListener{
-         val intent = Intent(this,CustomListView::class.java)//Explicit Intent
-            startActivity(intent)
-            Toast.makeText(this,"Clicked",Toast.LENGTH_SHORT).show()
-        }
-        RecycleViewbt.setOnClickListener{
-         val intent = Intent(this,RecycleViewPage::class.java)//Explicit Intent
-            startActivity(intent)
-            Toast.makeText(this,"Clicked",Toast.LENGTH_SHORT).show()
-        }
-        Fragmentlayoutbt.setOnClickListener{
-         val intent = Intent(this,FragmentPage::class.java)//Explicit Intent
-            startActivity(intent)
-            Toast.makeText(this,"Clicked",Toast.LENGTH_SHORT).show()
-        }
-        Customdialogbt.setOnClickListener {
-            val message= "Do you really want to logout ??"
-            showdialog(message)
-        }
-
-        BottomSheetdialogbt.setOnClickListener {
-            val dialog = BottomSheetDialog(this)
-            val view  = layoutInflater.inflate(R.layout.bottom_sheet_dialog,null)
-            val btnclose =  view.findViewById<Button>(R.id.idBtnDismiss)
-            btnclose.setOnClickListener{
-                dialog.dismiss()
-            }
-            dialog.setCancelable(false)
-            dialog.setContentView(view)
-            dialog.show()
-        }
-
-        implicitbt.setOnClickListener{
-            val url = "https://www.geeksforgeeks.org/implicit-and-explicit-intents-in-android-with-examples/"
-         val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))//Implicit Intent
-            startActivity(intent)
-            Toast.makeText(this,"Clicked",Toast.LENGTH_SHORT).show()
-        }
+        // to make the Navigation drawer icon always appear on the action bar
+       // supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        replaces(fragment = HomeFragment())
 
 
     }
 
-    private fun showdialog(message: String) {
-        val dialog = Dialog(this)
-        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
-        dialog.setCancelable(false)
-        dialog.setContentView(R.layout.custom_dialog_box)
-        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-
-        val tvMessage:TextView = dialog.findViewById(R.id.titletxt)
-        val yesbtn:TextView = dialog.findViewById(R.id.yesbtn)
-        val nobtn:TextView = dialog.findViewById(R.id.nobtn)
-
-        tvMessage.setText(message)
-        yesbtn.setOnClickListener{
-            Toast.makeText(this,"Clicked Yes",Toast.LENGTH_SHORT).show()
-        }
-        nobtn.setOnClickListener{
-            dialog.dismiss()
-        }
-        dialog.show()
+    private fun replaces(fragment: HomeFragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragment_container,fragment)
+        fragmentTransaction.commit()
 
     }
+
+//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//        return if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+//            true
+//        } else super.onOptionsItemSelected(item)
+//    }
+
+
 
 }
---
+++ app/src/main/java/com/pratice/android_tutorial/fragment/HomeFragment.kt
@@ -0,0 +1,147 @@
+package com.pratice.android_tutorial.fragment
+
+import android.app.Dialog
+import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.net.Uri
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.Window
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.pratice.android_tutorial.ConstraintLayout
+import com.pratice.android_tutorial.CustomListView
+import com.pratice.android_tutorial.FrameLayout
+import com.pratice.android_tutorial.LinearLayouts
+import com.pratice.android_tutorial.ListViewfile
+import com.pratice.android_tutorial.R
+import com.pratice.android_tutorial.RecycleViewPage
+import com.pratice.android_tutorial.RelativelLayouts
+import com.pratice.android_tutorial.ViewElements
+
+class HomeFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for activity fragment
+        val view  =  inflater.inflate(R.layout.fragment_home, container, false)
+        val linearLayoutbtn: Button = view.findViewById(R.id.Linearlayoutbt)
+        val RelativelLayoutbtn: Button = view.findViewById(R.id.Relativelayoutbt)
+        val ConstraintLayouts: Button = view.findViewById(R.id.constraintlayoutbt)
+        val FrameLayouts: Button = view.findViewById(R.id.framelayoutbt)
+        val viewElementlayoutbt: Button = view.findViewById(R.id.viewElementlayoutbt)
+        val implicitbt: Button = view.findViewById(R.id.implicitbt)
+        val ListViewbt: Button = view.findViewById(R.id.ListViewbt)
+        val CustListViewbt: Button = view.findViewById(R.id.CustListViewbt)
+        val RecycleViewbt: Button = view.findViewById(R.id.RecycleViewbt)
+        val Fragmentlayoutbt: Button = view.findViewById(R.id.Fragmentlayoutbt)
+        val Customdialogbt: Button = view.findViewById(R.id.Customdialogbt)
+        val BottomSheetdialogbt: Button = view.findViewById(R.id.BottomSheetdialogbt)
+
+        linearLayoutbtn.setOnClickListener{
+            val intent = Intent(activity, LinearLayouts::class.java)//Explicit Intent
+            startActivity(intent)
+            Toast.makeText(activity,"Clicked", Toast.LENGTH_SHORT).show()
+        }
+        RelativelLayoutbtn.setOnClickListener{
+            val intent = Intent(activity, RelativelLayouts::class.java)//Explicit Intent
+            startActivity(intent)
+            Toast.makeText(activity,"Clicked", Toast.LENGTH_SHORT).show()
+        }
+        ConstraintLayouts.setOnClickListener{
+            val intent = Intent(activity, ConstraintLayout::class.java)//Explicit Intent
+            startActivity(intent)
+            Toast.makeText(activity,"Clicked", Toast.LENGTH_SHORT).show()
+        }
+        FrameLayouts.setOnClickListener{
+            val intent = Intent(activity, FrameLayout::class.java)//Explicit Intent
+            startActivity(intent)
+            Toast.makeText(activity,"Clicked", Toast.LENGTH_SHORT).show()
+        }
+        viewElementlayoutbt.setOnClickListener{
+            val intent = Intent(activity, ViewElements::class.java)//Explicit Intent
+            startActivity(intent)
+            Toast.makeText(activity,"Clicked", Toast.LENGTH_SHORT).show()
+        }
+        ListViewbt.setOnClickListener{
+            val intent = Intent(activity, ListViewfile::class.java)//Explicit Intent
+            startActivity(intent)
+            Toast.makeText(activity,"Clicked", Toast.LENGTH_SHORT).show()
+        }
+        CustListViewbt.setOnClickListener{
+            val intent = Intent(activity, CustomListView::class.java)//Explicit Intent
+            startActivity(intent)
+            Toast.makeText(activity,"Clicked", Toast.LENGTH_SHORT).show()
+        }
+        RecycleViewbt.setOnClickListener{
+            val intent = Intent(activity, RecycleViewPage::class.java)//Explicit Intent
+            startActivity(intent)
+            Toast.makeText(activity,"Clicked", Toast.LENGTH_SHORT).show()
+        }
+        Fragmentlayoutbt.setOnClickListener{
+            val intent = Intent(activity,FragmentPage::class.java)//Explicit Intent
+            startActivity(intent)
+            Toast.makeText(activity,"Clicked", Toast.LENGTH_SHORT).show()
+        }
+        Customdialogbt.setOnClickListener {
+            val message= "Do you really want to logout ??"
+            showdialog(message)
+        }
+
+        BottomSheetdialogbt.setOnClickListener {
+            val dialog = BottomSheetDialog(requireActivity())
+            val view  = layoutInflater.inflate(R.layout.bottom_sheet_dialog,null)
+            val btnclose =  view.findViewById<Button>(R.id.idBtnDismiss)
+            btnclose.setOnClickListener{
+                dialog.dismiss()
+            }
+            dialog.setCancelable(false)
+            dialog.setContentView(view)
+            dialog.show()
+        }
+
+        implicitbt.setOnClickListener{
+            val url = "https://www.geeksforgeeks.org/implicit-and-explicit-intents-in-android-with-examples/"
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))//Implicit Intent
+            startActivity(intent)
+            Toast.makeText(activity,"Clicked", Toast.LENGTH_SHORT).show()
+        }
+     
+        return view
+
+    }
+
+    private fun showdialog(message: String) {
+        val dialog = Dialog(requireActivity())
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setCancelable(false)
+        dialog.setContentView(R.layout.custom_dialog_box)
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        val tvMessage: TextView = dialog.findViewById(R.id.titletxt)
+        val yesbtn: TextView = dialog.findViewById(R.id.yesbtn)
+        val nobtn: TextView = dialog.findViewById(R.id.nobtn)
+
+        tvMessage.setText(message)
+        yesbtn.setOnClickListener{
+            Toast.makeText(activity,"Clicked Yes",Toast.LENGTH_SHORT).show()
+        }
+        nobtn.setOnClickListener{
+            dialog.dismiss()
+        }
+        dialog.show()
+
+    }
+}
