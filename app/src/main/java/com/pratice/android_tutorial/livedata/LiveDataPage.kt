ac8f940e4e9e2190c283b530cad6601e5595440d
Dj1752 <83537437+Dj1752@users.noreply.github.com>
2025-05-27 23:55:36 +0530

    LiveData in Android Kotlin

---
+++ app/src/main/java/com/pratice/android_tutorial/livedata/LiveDataPage.kt
@@ -0,0 +1,17 @@
+package com.pratice.android_tutorial.livedata
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.pratice.android_tutorial.R
+
+class LiveDataPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_live_data_page)
+
+    }
+}
