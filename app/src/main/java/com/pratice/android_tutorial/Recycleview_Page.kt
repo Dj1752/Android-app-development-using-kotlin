775af9f61fc80eea5c482f498b2967b77938f866
Dj1752 <83537437+Dj1752@users.noreply.github.com>
2025-06-02 23:51:01 +0530

    Recycleview_Page with MVVM in Android Kotlin

---
+++ app/src/main/java/com/pratice/android_tutorial/Recycleview_Page.kt
@@ -0,0 +1,16 @@
+package com.pratice.android_tutorial
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Recycleview_Page : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_recycleview_page)
+
+    }
+}
